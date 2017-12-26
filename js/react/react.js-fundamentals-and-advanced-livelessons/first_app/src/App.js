fcfb0a7be140a57e7ffde0a562e66df35437f064
Mohan Raj Rajamanickam <mohanraj.cit@gmail.com>
2017-12-26 11:56:51 -0800

    building a basic profiles component

--- js/react/react.js-fundamentals-and-advanced-livelessons/first_app/src/App.js
+++ js/react/react.js-fundamentals-and-advanced-livelessons/first_app/src/App.js
@@ -1,20 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class Profile extends Component {
+    render() {
+        let hobbies = this.props.hobbies.map((hobby, index) =>
+            <li key={index}>
+                {hobby}
+            </li>
+        );
+
+        return (
+            <div>
+                <h3>{this.props.name}</h3>
+                <p>{this.props.name} is {this.props.age} years old and {this.props.bio}</p>
+                <h3>Hobbies</h3>
+                <ul>
+                    {hobbies}
+                </ul>
+            </div>
+        );
+    }
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            profiles : [
+                {
+                    name: "Sue",
+                    age: 30,
+                    bio: "enjoys outdoor activities",
+                    hobbies: ["swimming", "biking"]
+                },
+                {
+                    name: "Bill",
+                    age: 40,
+                    bio: "enjoys reading books",
+                    hobbies: ["gardening", "games"]
+                }
+            ]
+        };
+    }
+
+    render() {
+        let profiles = this.state.profiles.map((profile, index) =>
+                <Profile
+                    key={index}
+                    name={profile.name}
+                    age={profile.age}
+                    bio={profile.bio}
+                    hobbies={profile.hobbies}
+                />
+        );
+
+        return (
+          <div className="App">
+            <header className="App-header">
+              <h1 className="App-title">Profiles</h1>
+            </header>
+              <div>
+                  {profiles}
+              </div>
+          </div>
+        );
   }
 }
 
