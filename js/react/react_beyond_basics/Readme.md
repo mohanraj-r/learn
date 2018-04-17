7792d76df5e927ceb48e2027e181fef9ad282a6b
Mohan Raj Rajamanickam <mohanraj.cit@gmail.com>
2018-04-17 11:17:58 -0700

    Add React beyond basics training

---
+++ js/react/react_beyond_basics/Readme.md
@@ -0,0 +1,2 @@
+## Resources
+* [jscomplete/react-virtual-dom-demo: React's Virtual DOM demo](https://github.com/jscomplete/react-virtual-dom-demo)
---
+++ js/react/react_beyond_basics/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const jsContainer = document.getElementById("js");
+const reactContainer = document.getElementById("react");
+
+const render = () => {
+    jsContainer.innerHTML = `
+    <div class="demo">
+      Hello JS
+      <input />
+      <p>${new Date()}</p>
+    </div>
+  `;
+
+    ReactDOM.render(
+        React.createElement(
+            "div",
+            { className: "demo" },
+            "Hello React ",
+            React.createElement("input"),
+            React.createElement(
+                "p",
+                null,
+                new Date().toString()
+            )
+        ),
+        reactContainer
+    );
+}
+
+setInterval(render, 1000);
