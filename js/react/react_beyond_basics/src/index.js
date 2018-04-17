98e6c9b9d9f80ba0919b3581f1c16d19bd28740c
Mohan Raj Rajamanickam <mohanraj.cit@gmail.com>
2018-04-17 11:33:04 -0700

    Convert to JSX from react api
    remove js compare container

--- js/react/react_beyond_basics/src/index.js
+++ js/react/react_beyond_basics/src/index.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const jsContainer = document.getElementById("js");
 const reactContainer = document.getElementById("react");
 
 const render = () => {
-    jsContainer.innerHTML = `
-    <div class="demo">
-      Hello JS
-      <input />
-      <p>${new Date()}</p>
-    </div>
-  `;
-
     ReactDOM.render(
-        React.createElement(
-            "div",
-            { className: "demo" },
-            "Hello React ",
-            React.createElement("input"),
-            React.createElement(
-                "p",
-                null,
-                new Date().toString()
-            )
-        ),
+        <div className={"demo"}>
+            Hello World!
+            <input/>
+            <p>{new Date().toString()}</p>
+        </div>,
         reactContainer
     );
 }
